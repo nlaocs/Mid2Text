f702d1d468a2df8eab67731e2c56e2a063bfdbb6
あお <113911070+nlaocs@users.noreply.github.com>
2025-04-16 13:41:05 +0900

    struct Noteにnew, key_to_char, to_char実装

--- mid_text_converter/src/instruments.rs
+++ mid_text_converter/src/instruments.rs
@@ -5,3 +5,25 @@ pub struct Note {
     pub key: u7,
     pub start_timing: u32,
 }
+
+impl Note {
+    pub fn new(key: u7, start_timing: u32) -> Self {
+        Self { key, start_timing }
+    }
+
+    pub fn key_to_char(key: u8) -> Option<char> {
+        if key < 54 && key > 78 {
+            // todo error
+            return None;
+        };
+        let mut k = key;
+        k += 11;
+        let char = char::from(k);
+        Some(char)
+    }
+
+    pub fn to_char(&self) -> char {
+        Self::key_to_char(self.key.as_int()).unwrap()
+    }
+}
+
